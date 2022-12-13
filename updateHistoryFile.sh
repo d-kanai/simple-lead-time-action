f29b4b57adf74a6718cc5f9f59b4c318a98531a7
simple-lead-time-action <simple-lead-time-action>
2022-12-13 19:21:17 +0900

    working

--- updateHistoryFile.sh
+++ updateHistoryFile.sh
@@ -4,9 +4,8 @@ set -e
 LEAD_TIME=$1
 PR_LINK=$2
 
-not_exist_branch=`git ls-remote --heads origin ${SAVE_HISTORY_FILE_BRANCH}`
-if [[ -z ${not_exist_branch} ]]; then
-#  git branch -d ${SAVE_HISTORY_FILE_BRANCH}
+not_exist_branch=$(git ls-remote --heads origin ${SAVE_HISTORY_FILE_BRANCH})
+if [ -z ${not_exist_branch} ]; then
   git branch ${SAVE_HISTORY_FILE_BRANCH}
   git checkout ${SAVE_HISTORY_FILE_BRANCH}
   git push --set-upstream origin ${SAVE_HISTORY_FILE_BRANCH}
