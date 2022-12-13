b0c3d36bd5f552e14f92c6906c43a4b7e830f2ce
simple-lead-time-action <simple-lead-time-action>
2022-12-13 13:21:35 +0900

    auto commit by simple-lead-time-action

--- commit.sh
+++ commit.sh
@@ -10,7 +10,6 @@ git config --local user.email "simple-lead-time-action"
 git config --local user.name "simple-lead-time-action"
 git add -A
 git commit -m "auto commit by simple-lead-time-action"
-echo 'commited'
+echo 'git commited'
 git push "${remote_repo}" HEAD:"${INPUT_BRANCH}";
-
-echo 'DONE'
+echo 'git pushed'
